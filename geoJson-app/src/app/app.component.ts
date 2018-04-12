71767745ae5c7d63496b810dead0aa655f4e43b7
mio g <mio.geez@gmail.com>
2018-04-12 16:26:47 +0800

    Update
    added loading message during API call

--- geoJson-app/src/app/app.component.ts
+++ geoJson-app/src/app/app.component.ts
@@ -19,22 +19,28 @@ export class AppComponent implements OnInit {
   private chart: any
   public startdate: string
   public enddate: string
+  public logMessage: string
 
   constructor(private http: HttpClient, private geojson: GeoJson, private geoparser: GeoParser) {
     //arbitrary default dates
     this.startdate = "1991-06-01"
     this.enddate = "1991-06-30"
+    this.logMessage = ""
 
   }
   ngOnInit() {
     this.chart = this.makeChart()
   }
   plotQuakes() {
+    this.logMessage = "Fetching earthquake data."
     this.geojson.getData(this.startdate, this.enddate)
       .subscribe((data) => {
+        this.logMessage = "Parsing earthquake paramaters."
         let newData = this.geoparser.toAmChart(data)
         this.chart.dataProvider.images = newData;
+        this.logMessage = "Plotting earthquake data."
         this.chart.validateData();
+        this.logMessage = ""
       })
   }
 
